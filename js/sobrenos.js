da5123a84bb836eb3160ccc7410465ba64671efc
ricardo4457 <57718264+ricardo4457@users.noreply.github.com>
2022-12-28 19:06:44 +0000

    Add files via upload

--- js/sobrenos.js
+++ js/sobrenos.js
@@ -2,7 +2,7 @@ function GotoPS() {
    document.getElementById("maps").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3003.3720289345447!2d-8.341420084580943!3d41.170052179284674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd248fb063345f2b%3A0x266808b41d99ca24!2sFlor%20do%20T%C3%A2mega!5e0!3m2!1spt-PT!2spt!4v1670427108630!5m2!1spt-PT!2spt"   ;
  }
  function GotoRM() {
-   document.getElementById("maps").src =  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3019.967057894532!2d-8.575242084592148!3d40.80671767932176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd23856e38093f1d%3A0x68549baec59807eb!2sFlor%20do%20T%C3%A2mega!5e0!3m2!1spt-PT!2spt!4v1670440346988!5m2!1spt-PT!2spt" ;
+   document.getElementById("maps").src =  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3006.1424597235778!2d-8.265314799999999!3d41.109579700000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd249aff54b89de1%3A0x5fbd40c9b341697f!2sAv.%20Cans%20411!5e0!3m2!1spt-PT!2spt!4v1672136818452!5m2!1spt-PT!2spt" ;
  }
  function GotoP() {
   document.getElementById("maps").src =  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48054.84300631209!2d-8.37544874649901!3d41.168837983881275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd248f7b6ab02f43%3A0xfca080bd3e085484!2sFlor%20do%20Tamega!5e0!3m2!1spt-PT!2spt!4v1670511544522!5m2!1spt-PT!2spt" ;
@@ -13,4 +13,11 @@ function GotoSV() {
 
    
 }
+
+function GotoA() {
+  document.getElementById("maps").src =  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10158.904073128027!2d-8.580725195813173!3d40.80027450489465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd239b5527f30ff9%3A0xbedde3f2dd865641!2s3860-136%20Avanca!5e0!3m2!1spt-PT!2spt!4v1672137601587!5m2!1spt-PT!2spt" ;
+
+   
+}
+
 
